380b3ecd574e557b18d49589df952aff5edef05b
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-24 20:07:27 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -35,7 +35,7 @@ def bulk_tweet_shortener(array)
   array.each {|x| puts(x)}
 end
 
-def selective_tweet_shortener(tweet) 
+def selective_tweet_shortener(tweet)
     if tweet.length > 140
       tweet = word_substituter(tweet)
     end
